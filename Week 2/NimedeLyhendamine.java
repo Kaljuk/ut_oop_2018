55b94b5639f55d532bea537ea4911a3a1bc248f0
Erik K <erik.kaljumae@gmail.com>
2018-02-25 04:05:14 +0200

    Teise ülesande lisamine

---
+++ Week 2/NimedeLyhendamine.java
@@ -0,0 +1,11 @@
+/**
+ * NimedeLyhendamine
+ */
+public class NimedeLyhendamine {
+    public static void main(String[] args) throws Exception {
+        
+    }
+    public String[] readPersons() {
+        
+    };
+}
