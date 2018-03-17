c19766ad927952bb0942df6681a891c1e6b6c449
Erik K <erik.kaljumae@gmail.com>
2018-03-17 18:47:14 +0200

    NimedeLyhendamise parandus - no FileReader & return null on FNF

--- Week 2/NimedeLyhendamine_parandus.java
+++ Week 2/NimedeLyhendamine_parandus.java
@@ -2,8 +2,12 @@
  * NimedeLyhendamine
  * 
  * Course of actions
- * Used links
+ * Used | useful links
+ * https://stackoverflow.com/questions/10730413/filenotfoundexception-when-using-bufferedreader-on-a-file
  * https://stackoverflow.com/questions/22749096/efficient-way-to-read-a-file-java?rq=1
+ * https://stackoverflow.com/questions/3478222/java-how-to-do-pythons-try-except-else
+ * https://stackoverflow.com/questions/23375515/im-getting-an-error-exception-filenotfoundexception-is-never-thrown-in-body-o
+ * 
  * Parandused
  * - Ei kasuta FileReaderit
  * 
@@ -26,16 +30,27 @@ public class NimedeLyhendamine_parandus {
         );
         // Process and print out the names
         printPersons(nimed);
+        readPersons("sellistFailiPole.txt");
     }
     
     // Get names from TXT file (nimed.txt)
     public static ArrayList<String> readPersons (String fn) throws Exception {
         // Save names here
         ArrayList<String> names = new ArrayList<String>();
+        InputStream inputStream;
 
-
-        InputStream is = new FileInputStream(new File(fn));
-        BufferedReader failiSisu = new BufferedReader(new InputStreamReader(is));
+        try {
+            inputStream = new FileInputStream(new File(fn));
+        } catch(FileNotFoundException e) {
+            //System.out.println("FNF");
+            return null;
+        }
+        
+        //InputStream inputStream = new FileInputStream(fn);
+        BufferedReader failiSisu;
+        // Ei kasuta fileReaderit ning saab kodeeringut valida
+        failiSisu = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+        
         // Get file contents
         String failiRida;
         while((failiRida= failiSisu.readLine()) != null) {
@@ -43,6 +58,8 @@ public class NimedeLyhendamine_parandus {
         }
         // Close the buffer
         failiSisu.close();
+        // When file
+
         // Return list of fullnames
         return names;
     }
